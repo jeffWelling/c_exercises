1b36eb60431e3bd4c3d94cfa261f858b9aa07d0d
Jeff Welling <jeff.welling@gmail.com>
2012-03-16 07:12:38 -0400

    Completed 1-7
    1-6 was skipped due to triviality

--- main.c
+++ main.c
@@ -2,18 +2,5 @@
 
 main()
 {
-    int fahr, celsius;
-    int upper, lower, step;
-
-    lower=0;
-    upper=300;
-    step=20;
-
-    fahr= upper;
-    printf("Celsius\tFahr\n");
-    while( fahr >= lower ){
-        celsius= 5 * (fahr-32) / 9;
-        printf("%d\t%d\n", celsius, fahr);
-        fahr=fahr-step;
-    }
+    printf("%d", EOF);
 }
