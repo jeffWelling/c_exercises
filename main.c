50767811e133ddf3a77d7affd959a7b514a80cbf
Jeff Welling <jeff.welling@gmail.com>
2012-03-19 23:11:55 -0400

    Finished  1-8 Added whatis.c

--- main.c
+++ main.c
@@ -2,9 +2,21 @@
 
 main()
 {
-    int i = 0;
-    while( i<=255 ){
-        printf("%c\t%d\n", i, i);
-        ++i;
+    int blanks =0;
+    int tabs =0;
+    int newlines=0;
+    int other=0;
+    int c;
+
+    while( (c=getchar()) != EOF ){
+        if(c==9) //9 is /t in ascii
+            tabs++;
+        else if(c==32)
+            blanks++;
+        else if (c==10)
+            newlines++;
+        else
+            other++;
     }
+    printf("Tabs:\t%d\nBlanks:\t%d\nNewlines:\t%d\nOther:\t%d\n", tabs, blanks, newlines, other);
 }
---
+++ whatis.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    int c;
+    c=getchar();
+    printf("\n%d\n%c\n",c,c);
+    return 0;
+}
