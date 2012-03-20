e9e05b5bfffd374ee3975ef762d522fe2e041518
Jeff Welling <jeff.welling@gmail.com>
2012-03-19 23:48:17 -0400

    Started 1-9

--- main.c
+++ main.c
@@ -1,22 +1,9 @@
 #include <stdio.h>
 
+// Write a program to copy its input to its output
+// replacing each tab with \t, backspace with \b and
+// backslash with //. This makes tabs and backspaces visible
+// in an unambiguous way
 main()
 {
-    int blanks =0;
-    int tabs =0;
-    int newlines=0;
-    int other=0;
-    int c;
-
-    while( (c=getchar()) != EOF ){
-        if(c==9) //9 is /t in ascii
-            tabs++;
-        else if(c==32)
-            blanks++;
-        else if (c==10)
-            newlines++;
-        else
-            other++;
-    }
-    printf("Tabs:\t%d\nBlanks:\t%d\nNewlines:\t%d\nOther:\t%d\n", tabs, blanks, newlines, other);
 }
