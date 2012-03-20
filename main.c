083077a97f9cdadc317a2658dad3149460d18592
Jeff Welling <jeff.welling@gmail.com>
2012-03-20 07:49:49 -0400

    Completed 1-9

--- main.c
+++ main.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
+#define IN 0
+#define OUT 1
 
+//Write a program that copies input to output replacing each
+// Series of spaces with one space
 main()
 {
-    long n;
-
-    n=0;
-    while(getchar() != EOF)
-        n++;
-    printf("%ld/n", n);
+    int c=0;
+    int space=0;
+    while( (c=getchar()) != EOF ){
+        if( c==32 ){
+            if( space==0 ){
+                printf(" ");
+                space=OUT;
+            } else if( space==1 ){
+                ;
+            }
+        } else {
+            printf("%c", c);
+            space=IN;
+        }
+    }
 }
