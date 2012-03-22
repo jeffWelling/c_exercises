752b106bbe1f31d04e04a6175e7beb8c9ffa94da
Jeff Welling <jeff.welling@gmail.com>
2012-03-22 07:27:54 -0400

    middle commit

--- main.c
+++ main.c
@@ -2,22 +2,43 @@
 #define IN 1
 #define OUT 0
 
-/* Write a program that prints its input one word per line */
+/* Write a program to print a histogram of the lengths of words in
+    itsinput. */
 main()
 {
-    int c=0;
+    int histogram[50];
+    int c;
+    int current_length=0;
     int state=OUT;
+
+    //Here we borrow C so we don't have to use i
+    printf("Initializing...");
+    for(c=0;c<=50;c++){
+        histogram[c]=0;
+    }
+    printf("Done\n");
+    
     while( (c=getchar()) != EOF){
-        if( state==OUT && (c>=32 && c<=126 )){
-            state=IN;
-            putchar(c);
-        } else if( state==IN && (c<=32 || c>=176 )){
+        if( (c==32 || c==10) && state==IN ){
+            //End of word
             state=OUT;
-            printf("\n");
-            putchar(c);
+            histogram[current_length++];
+        }else if( (c>=33 && c<=126) && state==OUT ){
+            //Start of word
+            state=IN;
+            current_length=0;
+        }else if( (c>=33 && c<=126) && state==IN ){
+            //In a word
+            current_length++;
         } else {
-            putchar(c);
+            //Not in a word
+            //Example, "  " or " \n "
+            ;
         }
     }
+
+    //Print the histogram
+    //Recycle current_length to hold the length of the longest word
+    for(     
 
 }
