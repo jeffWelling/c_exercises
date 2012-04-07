f776581be21f1cdb064fa0fe3643075a0580b0d6
Jeff Welling <jeff.welling@gmail.com>
2012-04-07 08:06:11 -0400

    Added test.txt and updated main.c to the example prog 'getline' after 1-15

--- main.c
+++ main.c
@@ -1,25 +1,50 @@
 #include <stdio.h>
+#define MAXLINE 100
 
-int fahr_to_cels( int fahr );
+int getaline( char line[], int maxline );
+void copy( char to[], char from[] );
 
+/* print the longest line */
 main()
 {
-    int fahr, celsius;
-    int upper, lower, step;
+    int len; // current line length
+    int max; // max line length
+    char line[MAXLINE]; // current line
+    char longest[MAXLINE]; // max line
 
-    lower=0;
-    upper=300;
-    step=20;
-
-    fahr= lower;
-    printf("Fahr\tCelsius\n");
-    while( fahr<= upper ){
-        celsius= fahr_to_cels(fahr);
-        printf("%d\t%d\n", fahr, celsius);
-        fahr=fahr+step;
+    max=0;
+    while((len = getaline(line, MAXLINE)) > 0){
+        if( len > max ){
+            max= len;
+            copy(longest,line);
+        }
     }
+    if( max > 0 ) //there was a line
+        printf("%s", longest);
+    return 0;
 }
 
-int fahr_to_cels( int fahr){
-    return (5*(fahr-32) /9);
+// getaline: read a line into s, return length
+int getaline( char s[], int limit )
+{
+    int c, i;
+
+    for( i=0; i < limit-1 && (c=getchar()) != EOF && c!='\n'; i++)
+            s[i]=c;
+
+    if( c=='\n') {
+        s[i]='\n';
+        i++;
+    }
+    s[i]='\0';
+    return i;
+}
+
+//copy: copy 'from' into 'to'
+void copy( char to[], char from[] )
+{
+    int i;
+    i=0;
+    while( (to[i] = from[i]) != '\0' )
+        i++;
 }
