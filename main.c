98f4783ff434b937f621df09d4949f0af8f291e3
Jeff Welling <jeff.welling@gmail.com>
2012-04-28 16:44:11 -0400

    Changed/Updated description to exercise 1-21

--- main.c
+++ main.c
@@ -7,10 +7,10 @@ int detab( char s[], int limit );
 int len=0;
 char s[MAXLINE];
 
-//1-20: Write a program detab that replaces tabs in the 
-//input with the proper number of blanks to space 
-//to the next tab stop. Assume a fixed set of tab 
-//stops, say every n columns.
+// 1-21: Write a program `entab` that replaces strings of blanks by the
+// minimum number of tabs and blanks to achieve the same spacing. Use
+// the same tab stops as for detab. When either a tab or a single blank
+// would suffice to reach a tab stop, which should be given preference?
 int main(){
     int len;
     char s[MAXLINE];
