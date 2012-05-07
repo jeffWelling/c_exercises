daa4961bf9b50a0eb93319bb2095f054ce1c2460
Jeff Welling <jeff.welling@gmail.com>
2012-05-06 22:51:49 -0400

    Completed 1-21

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #define MAXLINE 200
-#define TABSTOP 8
+#define TABSTOP 4
 int getline_( char s[], int limit);
-int detab( char s[], int limit );
+int entab( char s[], int limit );
+int seq_spaces( char s[], int i );
 
 int len=0;
 char s[MAXLINE];
@@ -12,21 +13,35 @@ char s[MAXLINE];
 // the same tab stops as for detab. When either a tab or a single blank
 // would suffice to reach a tab stop, which should be given preference?
 int main(){
-    int len;
     char s[MAXLINE];
     //assume tabstop every 4 spaces
     getline_( s, MAXLINE );
-    detab( s, MAXLINE );
+    entab( s, MAXLINE );
     printf( "%s", s );
+    return 0;
 }
 
-int detab( char s[], int limit ){
+int entab( char s[], int limit ){
     int s_i, ns_i, temp;;
     s_i= ns_i= temp= 0;
     char new_string[MAXLINE];
     while( s[s_i] != '\0' && s_i<=limit ){
-        if( s[s_i] == 9 ){
-            temp=ns_i;
+        //If spaces? > TABSTOP
+        //      delete_spaces(TABSTOP)
+        //      insert_tab(s)
+        if( s[s_i] == 32 ){
+            if( seq_spaces(s,s_i) >= TABSTOP ){
+                while( seq_spaces(s,s_i) >= TABSTOP ){
+                    s_i=s_i+TABSTOP;
+                    new_string[ns_i]='\t';
+                    ns_i++;
+                }
+            } else {
+                new_string[ns_i]=s[s_i];
+                ns_i++;
+                s_i++;
+            }
+            /*temp=ns_i;
             //If we're already on a tabstop boundary, increment
             if( (temp%TABSTOP) ==0 ){
                 new_string[ns_i]=' ';
@@ -38,7 +53,7 @@ int detab( char s[], int limit ){
                 ns_i++;
                 temp++;
             }
-            s_i++;
+            s_i++;*/
         } else {
             new_string[ns_i]=s[s_i];
             ns_i++;
@@ -53,6 +68,15 @@ int detab( char s[], int limit ){
     }
     s[ns_i]='\0';
     return ns_i;
+}
+
+int seq_spaces( char s[], int i ){
+	int counter=0;
+	while( s[i] == 32 ){
+		counter++;
+		i++;
+	}
+    return counter;
 }
 
 int getline_( char s[], int limit ){
