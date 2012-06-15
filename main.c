887e99621006706c33e3228005ea79326ae90a8d
Jeff Welling <jeff.welling@gmail.com>
2012-06-14 22:13:44 -0400

    Completed 1-23

--- main.c
+++ main.c
@@ -1,82 +1,35 @@
 #include <stdio.h>
+#include <string.h>
 #define MAXLINE 400
-#define TABSTOP 4
-#define MAX_COL 80
-int getline_( char s[], int limit);
-int fold( char s[], int limit);
-int last_nonblank( char s[], int s_i ); 
-int substrcp( char s[], int s_from, int s_to, char new_string[], int ns_i, int limit);
-void copy_str( char from[], int f_i, char to[], int t_i, int limit );
+int getline_( char s[], int limit );
+int strip_comments( char s[], int state );
+int substrcp( char s[], int s_from, int s_to, char new_string[], int ns_i, int limit );
+int comment_end( char s[], int i );
 
-//1-22. Write a program to ``fold'' long input lines into 
-//two or more shorter lines after the last non-blank character 
-//that occurs before the n-th column of input. Make sure your
-//program does something intelligent with very long lines, 
-//and if there are no blanks or tabs before the specified 
-//column.
-int main(){
-    char s[MAXLINE];
-    getline_( s, MAXLINE );
-    fold( s, MAXLINE );
-    printf( "%s", s );
-    return 0;
-}
 
-int fold( char s[], int limit ){
-    int s_i, ns_i, current_col;
-    int s_last_copy, last_space;
-    int num_copied;
-    char new_string[MAXLINE];
-    s_i= s_last_copy= num_copied= 0;
-    ns_i= current_col= 0;
-    //seek first non-blank character before nth column
-    while( s[s_i] != '\0' && s_i < limit && ns_i < limit ){
-        //at the end of a line, copy said line into new_string
-
-        if( current_col == MAX_COL ){
-            //find last space
-            last_space=last_nonblank(s, s_i);
-            last_space++; //so that we have the space after the last non-blank char
-
-            //copy s from s_last_copy to last_space into new_string
-            num_copied= substrcp( s, s_last_copy, last_space, new_string, ns_i, MAXLINE );
-            ns_i= ns_i + num_copied;
-            s_last_copy= last_space;
-
-            //insert newline into new_string, increment ns_i
-            new_string[ns_i]='\n';
-            ns_i++;
-            new_string[ns_i]='\0';
-            // s_i= last_space
-            s_i=last_space;
-            current_col=0;
-        } else {
-            current_col++;
-            s_i++;
-        }
+//Write a program to remove all comments from a C program. Don't forget to
+//handle quoted strings and character constants properly. C comments don't nest.
+int main()
+{
+    char string[MAXLINE];
+    int state;
+    state=0;
+    while( getline_(string,MAXLINE)!=0 ){
+        state=strip_comments( string, state );
+        printf( "%s", string );
     }
-    //copy last of s to new_string
-    last_space= last_nonblank(s, s_i);
-    if( s[s_i]!='\0' )
-        last_space++;
-    //need to make sure that substrcp doesn't copy past
-    //the end of new_string.
-    num_copied= substrcp( s, s_last_copy, MAXLINE, new_string, ns_i, MAXLINE );
-    ns_i= ns_i+num_copied;
-    new_string[ns_i]='\0';
-    s_i=last_space;
-    copy_str( new_string, 0, s, 0, MAXLINE );
     return 0;
 }
 
-int getline_( char s[], int limit ){
-    int c;
-    int i=0;
-    while( (c=getchar()) != EOF && c!= '\n' && i<limit ){
+int getline_( char s[], int limit)
+{
+    int i, c;
+    i=0;
+    while( (c=getchar()) != EOF && c != '\n' && i < limit-1 ){
         s[i]=c;
         i++;
     }
-    if( c=='\n' ){
+    if( c == '\n' ){
         s[i]=c;
         i++;
     }
@@ -84,24 +37,102 @@ int getline_( char s[], int limit ){
     return i;
 }
 
-int last_nonblank( char s[], int s_i ){
-    int found_blank=0;
-    if( s_i <= 0 )
-        return -1;
+int strip_comments( char s[], int state )
+{
+    int end_of_comment, ns_i, counter;
+    int s_i;
+    char new_string[MAXLINE];
+    end_of_comment= ns_i= s_i= 0;
+    // fubar fubar fubar /*fubar*/
+    /* fubar fubar
+       */
+    // /* fubar
+    */
+    /* //
+     */ //
+    
+    while( s[s_i] != '\0' && s[s_i] != '\n' ){
 
-    if( s[s_i]=='\0' )
-        return s_i;
+        if( state==0 ){
+            //not yet in a comment.
 
-    while( s_i >= 0 ){
-       if( found_blank==1 && s[s_i]!='\t' && s[s_i]!=32 )
-           return s_i+1;
+            //start of multi-line comment
+            if( s[s_i]=='/' && s[s_i+1] == '*' ){
+                state=1;
 
-       if( found_blank==0 && (s[s_i]=='\t' || s[s_i]==32))
-           found_blank=1;
+                //find end of comment
+                end_of_comment= comment_end( s, s_i );
+                // if end_of_comment == 0 then could not find end_of_comment
+                // ergo, it ends on one of the following lines.
 
-       s_i--;
-    }
-    return -1;
+                // copy s[i] until comment 
+                if( end_of_comment==0 ){
+                    //comment wraps to next line
+                    //copy s[0] to beginning of comment, return 1
+                    if( s_i != 0 ){
+                        counter=substrcp( s, 0, s_i, new_string, ns_i, MAXLINE-1 );
+                        ns_i+=counter;
+                    }
+                    new_string[ns_i]='\n';
+                    ns_i++;
+                    new_string[ns_i]='\0';
+                    strcpy( s, new_string );
+                    //we can return here because we know the comment ends on the next line
+                    //If we didn't know that, we would have to look for additional comments
+                    return 1;
+                } else {
+                    //comment ends on same line
+                    //copy s[0] to beginning of comment
+                    if( s_i != 0 ){
+                        counter= substrcp( s, 0, s_i, new_string, ns_i, MAXLINE );
+                        ns_i+=counter;
+                    }
+                    //copy s[end_of_comment] to '\0';
+                    if( end_of_comment!=( strlen(s)-1 ) ){
+                        counter= substrcp( s, end_of_comment, strlen(s), new_string, ns_i, MAXLINE-1 );
+                        ns_i+=counter;
+                    }
+                    new_string[ns_i]='\n';
+                    ns_i++;
+                    new_string[ns_i]='\0';
+                    strcpy( s, new_string );
+                    return 0;
+                }
+                   //start of single line comment
+            } else if( s[s_i]=='/' && s[s_i+1]=='/' ){
+                //copy from s[0] to s_i
+                if( s_i != 0 ){
+                    counter=substrcp( s, 0, s_i, new_string, ns_i, MAXLINE-1 );
+                    ns_i+=counter;
+                }
+                new_string[ns_i]='\n';
+                ns_i++;
+                new_string[ns_i]='\0';
+                strcpy( s, new_string );
+                return 0;
+            }
+        } else if( state==1 ){
+            //already in a multi-line comment from previous line
+            //find end of comment
+            end_of_comment= comment_end( s, 0 );
+            if( end_of_comment==0 ){
+                //comment continues to next line
+                s[0]='\n';
+                s[1]='\0';
+                return 1;
+            } else {
+                counter=substrcp( s, end_of_comment, strlen(s), new_string, ns_i, MAXLINE-1 );
+                ns_i+=counter;
+                new_string[ns_i]='\n';
+                ns_i++;
+                new_string[ns_i]='\0';
+                strcpy( s, new_string );
+                return 0;
+            }
+        }
+        s_i++;
+    } 
+    return state;
 }
 
 int substrcp( char s[], int s_from, int s_to, char new_string[], int ns_i, int limit ){
@@ -118,12 +149,13 @@ int substrcp( char s[], int s_from, int s_to, char new_string[], int ns_i, int l
     return counter;
 }
 
-void copy_str( char string[], int s_i, char new_string[], int ns_i, int limit ){
-    while( s_i<=limit && string[s_i]!='\0' && ns_i<=limit){
-        new_string[ns_i]=string[s_i];
-        ns_i++;
-        s_i++;
+int comment_end( char s[], int i )
+{
+    while( (s[i] != '*' && s[i+1] != '/') && s[i] != '\n' && s[i] != '\0' ){ 
+        i++;
     }
-    new_string[ns_i]='\0';
-    return;
+    if( s[i]=='*' && s[i+1]=='/' ){
+        return i+1;
+    }
+    return 0;
 }
