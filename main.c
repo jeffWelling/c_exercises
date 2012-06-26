3bdc805301605f8a29344bfdc6461e57cea3e021
Jeff Welling <jeff.welling@gmail.com>
2012-06-26 08:03:46 -0400

    Completed 2-4

--- main.c
+++ main.c
@@ -2,22 +2,22 @@
 #include <string.h>
 #define MAXLINE 400
 int getline_( char s[], int limit );
-int htoi( char s[] );
-
-static const long hextable[] = {
-    [0 ... 255] = -1,
-    ['0'] = 0,1,2,3,4,5,6,7,8,9,
-    ['A'] = 10,11,12,13,14,15,
-    ['a'] = 10,11,12,13,14,15
-};
+int squish( char string[], char gsub_chars[] );
+int string_has( char string[], char c );
 
 // Exercise 2-4. Write an alternative version of squeeze(s1,s2) that deletes each character in
 // s1 that matches any character in the string s2.
 //
-int main(){
-    char s[MAXLINE];
-    getline_( s, MAXLINE );
-    printf( "%d", htoi(s) );
+int main()
+{
+    char string[MAXLINE];
+    char remove[MAXLINE];
+    printf( "Please enter the string:\n" );
+    getline_( string, MAXLINE );
+    printf( "Please enter the characters to remove from string:\n" );
+    getline_( remove, MAXLINE );
+    squish( string, remove );
+    printf( "%s", string );
     return 0;
 }
 
@@ -25,9 +25,6 @@ int getline_( char s[], int limit)
 {
     int i, c;
     i=0;
-    for( i=0; (i<limit-1) + ((c=getchar())!='\n') + (c!=EOF) == 3; i++){
-           s[i]=c;
-    } 
     c=getchar();
     while( ((i<limit-1)+(c!='\n')+(c!=EOF))==3 ){
         s[i++]=c;
@@ -41,15 +38,29 @@ int getline_( char s[], int limit)
     return i;
 }
 
-int htoi( char s[] ){
-    int i=0;
-    long ret=0;
-    while( s[i]!='\n' && s[i]!=0 ){
-        if( i==0 && s[i]=='0' && (s[i+1]=='x' || s[i+1]=='X') ){
-            i+=2;
+int squish( char string[], char gsub_chars[] )
+{
+    int string_i=0;
+    int ns_i=0;
+    int len;
+    char new_string[MAXLINE];
+    for( len=strlen(string); string_i<=len; ){
+        if( string[string_i]!='\n' && string_has( gsub_chars, string[string_i] )==1 ){
+            string_i++;
         } else {
-            ret= (ret << 4) | hextable[ (int) s[i++] ];
+            new_string[ns_i++]=string[string_i++];
         }
     }
-    return ret;
+    strcpy( string, new_string );
+    return 0;
+}
+
+int string_has( char string[], char c )
+{
+    int i=0;
+    while( string[i]!='\0' ){
+        if( string[i++]==c )
+            return 1;
+    }
+    return 0;
 }
